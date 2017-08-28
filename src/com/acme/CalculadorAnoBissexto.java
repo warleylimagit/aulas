b69f70afeb48c5545c6df6543365b5e65e6c2867
Marco Mendes <marco.mendes@arkhi.com.br>
2017-08-28 20:17:44 -0300

    Projeto exemplo para demonstrar testes de unidade e integracao continua

---
+++ src/com/acme/CalculadorAnoBissexto.java
@@ -0,0 +1,21 @@
+package com.acme;
+
+public class CalculadorAnoBissexto {
+
+	public static boolean ehAnoBissexto(int ano) {
+		if (ano % 2 != 0) {
+			return false;
+		}
+		boolean divisivelPor4 = ano % 4 == 0;
+		boolean divisivelPor100 = ano % 100 == 0;
+		boolean divisivelPor400 = ano % 400 == 0;
+		
+		
+		if (divisivelPor4 && (!divisivelPor100  || divisivelPor400 )) {
+			return true;
+		}
+		return false;
+	}
+
+	
+}
---
+++ src/com/acme/teste/TesteCalculadorAnoBissexto.java
@@ -0,0 +1,37 @@
+package com.acme.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.acme.CalculadorAnoBissexto;
+
+public class TesteCalculadorAnoBissexto {
+
+	@Test
+	public void testeAnoImpar() {
+		boolean resultado = CalculadorAnoBissexto.ehAnoBissexto(1977);
+		Assert.assertFalse("Ano Impar",resultado);
+	}
+	@Test
+	public void testeAnoParNaoBissexto() {
+		boolean resultado = CalculadorAnoBissexto.ehAnoBissexto(1982);
+		Assert.assertFalse("Ano par não bissexto",resultado);
+	}
+	@Test
+	public void testeAnoBissextoTradicional() {
+		boolean resultado = CalculadorAnoBissexto.ehAnoBissexto(1984);
+		Assert.assertTrue("Ano bissexto tradicional",resultado);
+	}
+	@Test
+	public void testeAnoSeculoNaoBissexto() {
+		boolean resultado = CalculadorAnoBissexto.ehAnoBissexto(1900);
+		Assert.assertFalse("Ano de seculo nao bissexto",resultado);
+	}
+	@Test
+	public void testeAnoSeculoBissexto() {
+		boolean resultado = CalculadorAnoBissexto.ehAnoBissexto(2000);
+		Assert.assertTrue("Ano de seculo que eh bissexto",resultado);
+	}
+}
